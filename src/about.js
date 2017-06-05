e562ffc990c454dbd8fe4f46b07a1aac6fe66c68
Claudia Mane <c@luperlabs.com>
2017-06-04 23:21:04 -0700

    feat: Added definition for about us page

--- src/about.js
+++ src/about.js
@@ -9,7 +9,9 @@ class About extends Component {
   return (
   <Container>
     <Header>About Us</Header>
-    <p>Hello</p>
+    <p>Luper labs is a small collective of found life forms with a minimum amount of agency and free will.</p>
+    <p>None of us are responsible for the failures or damages of another member. We cannot vouch for another member's skill, ethics, or existence.</p>
+    <p>All related AI members have assured us that they are licensed under BSD or MIT, but they might be lying to hide a shameful GNU history. Please talk to a personal or company provided lawyer before engaging with any luper labs associated AI. We recommend that you do not share your personal phone number with any of them.</p>
   </Container>
   )
   }

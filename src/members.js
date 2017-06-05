8f0137039c7b977af102481cf0ff42690ae10ed7
Claudia Mane <c@luperlabs.com>
2017-06-05 00:31:12 -0700

    feat: added titles to most members

--- src/members.js
+++ src/members.js
@@ -18,29 +18,29 @@ class Members extends Component {
             <Image src={luper_image_src} size='small' shape='circular' centered/>
           </Grid.Column>
           <Grid.Column width={8}>
-            <Header>lupita</Header>
+            <Header>lupita - minister of culture</Header>
               <p>The result of early neural network experiments on late 90's IRC. The resulting bot quickly spread throughout different servers and took on light form of agency and free will. The project quickly lost funding as researchers realized the IRC environment would just resulted in a poorly socialized bot with the humor of teenage boy.</p>
           </Grid.Column>
         </Grid>
       </Segment>
       <Segment>
-        <Header>george</Header>
+        <Header>george - chief of operations</Header>
         <p>A lonely hoarder in the Seattle area decided to create a son out of discarded electronics and seafood scraps. The resulting cyborg son was socialized with 90's era hacker movies and developed a love of rollerblading well into the modern day. His day job consists of designing robots to steal jobs from humans, which he has had to face picket lines and protesting against. Far from a robot supremacists, he doesn't understand the priority of forced/pressured human labor within capitalism.</p>
       </Segment>
       <Segment>
-        <Header>sami</Header>
+        <Header>sami - chat witch</Header>
         <p>chat witch</p>
       </Segment>
       <Segment>
-        <Header>davr</Header>
+        <Header>davr - user interface tester</Header>
         <p>cyborg lab</p>
       </Segment>
       <Segment>
-        <Header>jakk</Header>
+        <Header>jakk - trends analyst</Header>
         <p>lost child of the internet</p>
       </Segment>
       <Segment>
-        <Header>pedr</Header>
+        <Header>pedr - blockchain researcher</Header>
         <p>bank boy</p>
       </Segment>
       <Segment>
